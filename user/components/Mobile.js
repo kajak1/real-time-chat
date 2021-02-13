62e861b70ff51f064c6d00931bbfa5c536765c7b
kajak1 <kajetan.korzonkiewicz@gmail.com>
2021-02-13 18:40:45 +0100

    removed comment

--- user/components/Mobile.js
+++ user/components/Mobile.js
@@ -50,32 +50,3 @@ const Mobile = () => {
 };
 
 export default Mobile;
-
-// <form
-//   className='create-room-form'
-//   onSubmit={(e) => {
-//     e.preventDefault();
-//     if (/\W+/.test(roomName)) {
-//       return;
-//     }
-//     socket.emit('create room', { roomName });
-//     setRoomName('');
-//   }}>
-//   <div className='wrapper'>
-//     <input
-//       id='room-name'
-//       name='room-name'
-//       type='text'
-//       value={roomName}
-//       onChange={(e) => {
-//         setRoomName(e.target.value);
-//       }}
-//       autoComplete='off'
-//       required
-//     />
-//     <label className='input-label' htmlFor='room-name'>
-//       room name
-//     </label>
-//   </div>
-//   <button type='submit'>Create Room</button>
-// </form>
