724df0dd8bb480a9d5056c2341d2c6bc720ea60f
kajak1 <kajetan.korzonkiewicz@gmail.com>
2021-02-13 18:42:25 +0100

    anonymous onClick -> func declaration

--- user/components/desktop/Room.js
+++ user/components/desktop/Room.js
@@ -5,6 +5,21 @@ import UserContext from '../../utils/UserContext';
 export default function Room({ roomName }) {
   const [userInfo, setUserInfo] = useContext(UserContext);
 
+  function handleJoinClick(roomName) {
+    if (roomName === userInfo.activeRoom) return;
+    const tmpName = userInfo.username;
+    setUserInfo({ username: tmpName, activeRoom: roomName });
+    socket.emit('join room', {
+      roomName: roomName,
+      activeRoomName: userInfo.activeRoom,
+    });
+  }
+
+  function handleDeleteClick(roomName) {
+    if (roomName === 'global') return;
+    socket.emit('delete room', { roomName: roomName });
+  }
+
   return (
     <li>
       {roomName}
@@ -12,26 +27,10 @@ export default function Room({ roomName }) {
         onSubmit={(e) => {
           e.preventDefault();
         }}>
-        <button
-          type='submit'
-          onClick={() => {
-            if (roomName === userInfo.activeRoom) return;
-            const tmpName = userInfo.username;
-            setUserInfo({ username: tmpName, activeRoom: roomName });
-            socket.emit('join room', {
-              roomName: roomName,
-              activeRoomName: userInfo.activeRoom,
-            });
-          }}>
+        <button type='submit' onClick={() => handleJoinClick(roomName)}>
           join
         </button>
-        <button
-          onClick={() => {
-            if (roomName === 'global') return;
-            socket.emit('delete room', { roomName: roomName });
-          }}>
-          delete
-        </button>
+        <button onClick={() => handleDeleteClick(roomName)}>delete</button>
       </form>
     </li>
   );
