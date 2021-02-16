bf2a70094c87bc51e695c6c2b703a3e5c45687fa
kajak1 <kajetan.korzonkiewicz@gmail.com>
2021-02-16 18:48:25 +0100

    dependency update

--- server/server.js
+++ server/server.js
@@ -1,60 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = 9999;
-const Users = require('./Users');
-const Rooms = require('./Rooms');
+const Users = require("./Users");
+const Rooms = require("./Rooms");
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 const users = new Users();
 const rooms = new Rooms();
 
-io.on('connection', (socket) => {
-  socket.on('user login', ({ username }) => {
+io.on("connection", (socket) => {
+  socket.on("user login", ({ username }) => {
     users.add(socket.id, username);
-    socket.join('global');
-    rooms.addUser(socket.id, 'global');
+    socket.join("global");
+    rooms.addUser(socket.id, "global");
     console.log(`${socket.id} joined global room`);
 
-    socket.emit('startup', {
+    socket.emit("startup", {
       allMsg: rooms.global.messages,
       users: rooms.global.users,
       rooms: rooms.getAllRooms(),
     });
 
-    socket.to('global').broadcast.emit('users update', {
+    socket.to("global").broadcast.emit("users update", {
       users: rooms.global.users,
     });
   });
 
-  socket.on('get startup', () => {
+  socket.on("get startup", () => {
     const roomName = users[socket.id].activeRoom;
-    socket.emit('startup', {
+    socket.emit("startup", {
       allMsg: rooms[roomName].messages,
       users: rooms[roomName].users,
       rooms: rooms.getAllRooms(),
     });
   });
 
-  socket.on('user typing', ({ isTyping }) => {
+  socket.on("user typing", ({ isTyping }) => {
     const username = users[socket.id].username;
     const activeRoom = users[socket.id].activeRoom;
-    socket.to(activeRoom).broadcast.emit('user typing', { isTyping, username });
+    socket.to(activeRoom).broadcast.emit("user typing", { isTyping, username });
   });
 
-  socket.on('chat update', ({ message }) => {
+  socket.on("chat update", ({ message }) => {
     const username = users[socket.id].username;
     const activeRoom = users[socket.id].activeRoom;
     rooms.addMessage({ username, message }, activeRoom);
 
-    io.to(activeRoom).emit('chat update', {
+    io.to(activeRoom).emit("chat update", {
       allMsg: rooms[activeRoom].messages,
     });
   });
 
-  socket.on('join room', ({ roomName, activeRoomName }) => {
+  socket.on("join room", ({ roomName, activeRoomName }) => {
     socket.leave(users[socket.id].activeRoom);
     socket.join(roomName);
     users.changeRoom(socket.id, roomName);
@@ -62,40 +62,40 @@ io.on('connection', (socket) => {
     rooms.addUser(socket.id, roomName);
 
     // update users in new room
-    socket.to(activeRoomName).broadcast.emit('users update', {
+    socket.to(activeRoomName).broadcast.emit("users update", {
       users: rooms[activeRoomName].users,
     });
 
     // update users in prev room
-    socket.to(roomName).broadcast.emit('users update', {
+    socket.to(roomName).broadcast.emit("users update", {
       users: rooms[roomName].users,
     });
 
     // get all info in new room and send it to client
-    socket.emit('startup', {
+    socket.emit("startup", {
       allMsg: rooms[roomName].messages,
       users: rooms[roomName].users,
       rooms: rooms.getAllRooms(),
     });
   });
 
-  socket.on('create room', ({ roomName }) => {
+  socket.on("create room", ({ roomName }) => {
     rooms.add(roomName);
-    io.emit('rooms update', { rooms: rooms.getAllRooms() });
+    io.emit("rooms update", { rooms: rooms.getAllRooms() });
   });
 
-  socket.on('delete room', ({ roomName }) => {
+  socket.on("delete room", ({ roomName }) => {
     rooms.delete(roomName);
-    io.emit('rooms update', { rooms: rooms.getAllRooms() });
+    io.emit("rooms update", { rooms: rooms.getAllRooms() });
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (Object.keys(users).length != 0) {
       console.log(`${socket.id} disconnected`);
     }
     rooms.removeUserFromRoom(socket.id, users[socket.id].activeRoom);
     users.remove(socket.id);
-    io.emit('users update', { users: rooms.global.users });
+    io.emit("users update", { users: rooms.global.users });
   });
 });
 
